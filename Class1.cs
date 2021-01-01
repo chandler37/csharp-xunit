61faf920d27ad66d1a17d30b453d0562dadd1840
chandler37 <30558411+chandler37@users.noreply.github.com>
2021-01-01 14:07:47 -0500

    Worked through an Xunit tutorial with .NET Core 5.0

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xunit;
+
+namespace csharp_xunit
+{
+    public class Class1
+    {
+    }
+}
+
+namespace SomeUnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(-1)]
+        public void MyTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1 || value % 2 == -1;
+        }
+    }
+}
+
